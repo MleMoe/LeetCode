ef8b26669822129a6b90ba5a39a5e9d74d044b61
MleMoe <i@mle.moe>
2021-10-19 17:53:15 +0800

    ✅ solve shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof

--- problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/solution.ts
+++ problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/solution.ts
@@ -1,13 +1,26 @@
+/**
+ * 一个整型数组 nums 里除两个数字之外，其他数字都出现了两次。请写程序找出这两个只出现一次的数字。
+ * 要求时间复杂度是O(n)，空间复杂度是O(1)。
+ * 思路：异或运算，相同值异或为 0，值与 0 异或 为原值
+ * @param nums
+ * @returns
+ */
 function singleNumbers(nums: number[]): number[] {
-	return Array.from(
-		nums.reduce((set, e) => {
-			if (set.has(e)) {
-				set.delete(e)
+	const n = nums.reduce((result, e) => result ^ e, 0)
+	let m = 1
+	while ((n & m) === 0) {
+		m <<= 1
+	}
+	return nums.reduce(
+		(results, e) => {
+			if ((e & m) > 0) {
+				results[0] ^= e
 			} else {
-				set.add(e)
+				results[1] ^= e
 			}
-			return set
-		}, new Set<number>())
+			return results
+		},
+		[0, 0]
 	)
 }
 
