7e0aed7d47f96850b94655d9fc9e9f9d39406ef3
MleMoe <i@mle.moe>
2022-04-03 20:36:19 +0800

    ✅ solve zigzag-conversion

--- problems/zigzag-conversion/solution.ts
+++ problems/zigzag-conversion/solution.ts
@@ -1,14 +1,36 @@
 /**
- * Z 字形变换
- * 寻找数规律
+ * 遍历+模拟，使用 numRows 大小的字符串数组辅助，确定行 index（分 -1/1 方向） 加入新字符。
+ * 特殊输入：numRows = 1，为原字符串
  * @param s
  * @param numRows
  */
 function convert(s: string, numRows: number): string {
-  return ''
+  if (numRows === 1) {
+    return s
+  }
+  const charList = new Array(numRows).fill('')
+  let flag = 1,
+    pos = 0
+  for (let i = 0; i < s.length; i++) {
+    charList[pos] += s[i]
+
+    if ((i / (numRows - 1)) % 2 === 0) {
+      flag = 1
+    }
+
+    if ((i / (numRows - 1)) % 2 === 1) {
+      flag = -1
+    }
+    pos += flag
+  }
+  return charList.join('')
 }
 
 import { testFunction } from '../../utils'
-const testCases = [['PAYPALISHIRING', 3]]
-const expectedResults = ['PAHNAPLSIIGYIR']
+const testCases = [
+  ['PAYPALISHIRING', 3],
+  ['PAYPALISHIRING', 4],
+  ['AB', 1],
+]
+const expectedResults = ['PAHNAPLSIIGYIR', 'PINALSIGYAHRPI', 'AB']
 testFunction(convert, testCases, expectedResults)
