4b8062e3563daf31147679a37e60ac80cf364c9c
MleMoe <i@mle.moe>
2022-04-03 16:24:18 +0800

    ✅ solve reverse-integer

---
+++ problems/reverse-integer/solution.ts
@@ -0,0 +1,26 @@
+/**
+ * 遍历，注意符号位，时间 O(n)，空间 O(1)
+ * @param x
+ * @returns
+ */
+function reverse(x: number): number {
+  let symbol = 1
+  if (x < 0) {
+    x = -x
+    symbol = -1
+  }
+  let num
+  let result = 0
+  while (x > 0) {
+    num = x % 10
+    x = Math.floor(x / 10)
+    result = result * 10 + num
+  }
+  result = result >= 2 ** 31 - 1 ? 0 : result
+  return result * symbol
+}
+
+import { testFunction } from '../../utils'
+const testCases = [[-123], [10200]]
+const expectedResults = [-321, 201]
+testFunction(reverse, testCases, expectedResults)
---
+++ problems/zigzag-conversion/solution.ts
@@ -0,0 +1,14 @@
+/**
+ * Z 字形变换
+ * 寻找数规律
+ * @param s
+ * @param numRows
+ */
+function convert(s: string, numRows: number): string {
+  return ''
+}
+
+import { testFunction } from '../../utils'
+const testCases = [['PAYPALISHIRING', 3]]
+const expectedResults = ['PAHNAPLSIIGYIR']
+testFunction(convert, testCases, expectedResults)
